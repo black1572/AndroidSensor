4b3bad647d58fd2d514f6b3265203b3c1b1001f0
cjw <1966261131@qq.com>
2023-11-16 15:34:19 +0800

    Modify commit first modify

--- app/src/main/java/com/example/androidsensor/MainActivity.java
+++ app/src/main/java/com/example/androidsensor/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.androidsensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView x,y,z;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        x = findViewById(R.id.x);
+        y = findViewById(R.id.y);
+        z = findViewById(R.id.z);
+
+        //获取传感器管理器 SensorManager
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        //获取加速度传感器 Sensor
+        Sensor accelerometerSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //定义传感器事件监听器
+        SensorEventListener accelerometerListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                //当
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        }
     }
 }
